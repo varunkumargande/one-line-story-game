b83af39419d994e5a5571f6174156f0670a942da
varun <babluvarun@gmail.com>
2024-02-10 01:09:34 +0100

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const PORT = 8000;
-const DB_USER_NAME = "one-line-story-game";
-const DB_USER_PWD = "IrYsa4VXcq4HRUUT";
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,9 +15,7 @@ const routes = require("./routes/index");
 app.use("/", routes);
 
 mongoose
-  .connect(
-    "mongodb+srv://one-line-story-game:IrYsa4VXcq4HRUUT@one-line-story-game.diezeg2.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGODB_URI)
   .then((result) => {
     app.listen(PORT);
   })
