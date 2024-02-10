9313d6157b28117c42c42d66a6fc4b9f58857cf3
varun <babluvarun@gmail.com>
2024-02-10 02:00:15 +0100

    vercel file

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
-const PORT = 8000;
+const PORT = 8080;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
